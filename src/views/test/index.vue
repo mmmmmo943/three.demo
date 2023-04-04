cf2f879a032735e630a21da6e214bbddc126ca29
mo chen <943154022@qq.com>
2023-04-04 17:49:21 +0800

    chf

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -8,19 +8,88 @@
 import { ref } from 'vue'
 // import { saveAs } from 'file-saver'
 import * as docx from 'docx'
-import { HeadingLevel, Paragraph, StyleLevel, TableOfContents } from 'docx'
+import imagePath from '../../assets/imgs/avatar.jpg'
+import {
+  File,
+  HeadingLevel,
+  Paragraph,
+  StyleLevel,
+  TableOfContents,
+  AlignmentType,
+  Table,
+  TableCell,
+  TableRow,
+  WidthType,
+  Footer,
+  Header,
+  NumberFormat,
+  PageNumber,
+  TextRun,
+  ImageRun
+} from 'docx'
 
 const documentName = ref('My Document')
 // const documentContent = ref('Hello, world!')
 
 // 创建用于创建文档的方法
-const createDocument = () => {
+const createDocument = async () => {
   const paragraph = new Paragraph({
     text: 'Hello World',
-    heading: HeadingLevel.HEADING_1
+    heading: HeadingLevel.HEADING_1,
+    pageBreakBefore: true,
+    alignment: AlignmentType.CENTER
   })
+  const table = new Table({
+    columnWidths: [3505, 5505],
+    rows: [
+      new TableRow({
+        children: [
+          new TableCell({
+            width: {
+              size: 3505,
+              type: WidthType.DXA
+            },
+            children: [new Paragraph('Hello')]
+          }),
+          new TableCell({
+            width: {
+              size: 5505,
+              type: WidthType.DXA
+            },
+            children: []
+          })
+        ]
+      }),
+      new TableRow({
+        children: [
+          new TableCell({
+            width: {
+              size: 3505,
+              type: WidthType.DXA
+            },
+            children: []
+          }),
+          new TableCell({
+            width: {
+              size: 5505,
+              type: WidthType.DXA
+            },
+            children: [new Paragraph('World')]
+          })
+        ]
+      })
+    ]
+  })
+
+  const response = await fetch(imagePath)
+  const arrayBuffer = await response.arrayBuffer()
+  const imageBytes = new Uint8Array(arrayBuffer)
+
   // 创建一个新的Document对象
   const doc = new docx.Document({
+    features: {
+      updateFields: true
+    },
     styles: {
       paragraphStyles: [
         {
@@ -33,32 +102,110 @@ const createDocument = () => {
             italics: true,
             color: '990000'
           }
+        },
+        {
+          id: 'MySpectacularStyle2',
+          name: 'My Spectacular Style2',
+          basedOn: 'Heading2',
+          next: 'Heading2',
+          quickFormat: true,
+          run: {
+            italics: true,
+            color: '930000'
+          }
         }
       ]
     },
     sections: [
       {
+        properties: {
+          page: {
+            pageNumbers: {
+              start: 1,
+              formatType: NumberFormat.DECIMAL
+            }
+          }
+        },
+        headers: {
+          default: new Header({
+            children: [
+              new Paragraph({
+                children: [
+                  new TextRun('Foo Bar corp. '),
+                  new TextRun({
+                    children: ['Page Number ', PageNumber.CURRENT]
+                  }),
+                  new TextRun({
+                    children: [' to ', PageNumber.TOTAL_PAGES]
+                  })
+                ]
+              })
+            ]
+          })
+        },
+        footers: {
+          default: new Footer({
+            children: [
+              new Paragraph({
+                alignment: AlignmentType.CENTER,
+                children: [
+                  new TextRun('Foo Bar corp. '),
+                  new TextRun({
+                    children: ['Page Number: ', PageNumber.CURRENT]
+                  }),
+                  new TextRun({
+                    children: [' to ', PageNumber.TOTAL_PAGES]
+                  })
+                ]
+              })
+            ]
+          })
+        },
         children: [
           new TableOfContents('Summary', {
             hyperlink: true,
-            headingStyleRange: '1-5',
-            stylesWithLevels: [new StyleLevel('MySpectacularStyle', 1)]
+            headingStyleRange: '1-2',
+            stylesWithLevels: [
+              new StyleLevel('MySpectacularStyle', 1),
+              new StyleLevel('MySpectacularStyle2', 2)
+            ]
           }),
           paragraph,
+          table,
           new Paragraph({
-            text: 'Header #1',
-            heading: HeadingLevel.HEADING_1
+            children: [
+              new ImageRun({
+                data: imageBytes,
+                transformation: {
+                  width: 100,
+                  height: 100
+                }
+              })
+            ]
+          }),
+          new Paragraph({
+            text: 'Header #1234',
+            heading: HeadingLevel.HEADING_1,
+            pageBreakBefore: true,
+            alignment: AlignmentType.CENTER,
+            style: 'MySpectacularStyle'
           }),
           new Paragraph("I'm a little text very nicely written.'"),
           new Paragraph({
             text: 'Header #2',
-            heading: HeadingLevel.HEADING_1
+            heading: HeadingLevel.HEADING_1,
+            pageBreakBefore: true,
+            alignment: AlignmentType.CENTER,
+            style: 'MySpectacularStyle'
           }),
           new Paragraph("I'm a other text very nicely written.'"),
           new Paragraph({
             text: 'Header #2.1',
-            heading: HeadingLevel.HEADING_2
+            heading: HeadingLevel.HEADING_2,
+            pageBreakBefore: true,
+            style: 'MySpectacularStyle2'
           }),
+
           new Paragraph("I'm a another text very nicely written.'"),
           new Paragraph({
             text: 'My Spectacular Style #1',
